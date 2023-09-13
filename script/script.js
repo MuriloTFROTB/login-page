48027d76cdb420d79579c72c52d5a56bf9f84adb
Murilo Pereira <pereiramurilo540@gmail.com>
2023-09-12 21:09:41 -0300

    mudando tudo

--- script/script.js
+++ script/script.js
@@ -2,11 +2,13 @@ const campName = document.getElementById("name");
 const labelName = document.getElementById("labelname");
 
 campName.addEventListener("focus", function() {
-  labelName.style.transform = "translateY(-50%)";
+  labelName.style.transform = "translateY(-100%)";
+  campName.style.border="2px solid red"
 });
 
 campName.addEventListener("blur", function() {
   if (!campName.value) {
-    labelName.style.transform = "translateY(0)";
+    labelName.style.transform = "translateY(40%)";
   }
+  campName.style.border="none"
 });
